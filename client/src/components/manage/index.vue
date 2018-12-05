31cdb35e1ce9f2a88eb5ad55f116427af4d3f2c5
qq <quyuan@zhaotai.net>
2018-12-05 18:28:04 +0800

    新增tinymce富文本编辑器

--- client/src/components/manage/index.vue
+++ client/src/components/manage/index.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <div>
-      <i class="iconfont icon-home"></i>
+    <div class="header">
+      <span><i class="iconfont icon-home"></i>返回主页</span>
+      <span>退出登录</span>
     </div>
     <div>
       <el-row>
-        <el-col :span="2" style="border:1px solid #ccc">
+        <el-col :span="2" class="side-nav">
           <div>
             <router-link to="/manage/allArticles">全部文章</router-link>
           </div>
@@ -13,10 +14,43 @@
             <router-link to="/manage/publish">发表文章</router-link>
           </div>
         </el-col>
-        <el-col :span="22">
+        <el-col :span="22" class="content-table">
           <router-view></router-view>
         </el-col>
       </el-row>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.header{
+  height: 5vh;
+  line-height: 5vh;
+  background: #3e4e62;
+  padding: 0 20px;
+  color: #fff;
+  border-bottom: 2px solid #fff;
+  i{
+    margin-right: 10px;
+  }
+  span{
+    cursor: pointer;
+  }
+  span:last-child{
+    float: right;
+  }
+}
+.side-nav{
+  background: #3e4e62;
+  height: 95vh;
+  padding-top: 20px;
+  border-right: 1px solid #ddd;
+  text-align: center;
+  line-height: 26px;
+  font-size: 14px;
+}
+.content-table{
+  padding: 30px;
+}
+</style>
+
---
+++ client/src/components/manage/publishArticle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <el-form>
+      <el-form-item label="标题">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item label="内容">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item label="上传图片">
+        <el-upload list-type="picture-card">
+          <i class="el-icon-plus"></i>
+        </el-upload>
+      </el-form-item>
+    </el-form>
+    <el-button type="primary">提交</el-button>
+  </div>
+</template>
